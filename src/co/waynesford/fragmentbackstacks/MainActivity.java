024404aea742b1acee1c3a24928ae5dbb6d3ad68
Wayne <monkeywayne@gmail.com>
2014-01-24 20:20:31 -0500

    refined bg_tab_selector, setting selected state now when clicked, removed calling add fragment

--- src/co/waynesford/fragmentbackstacks/MainActivity.java
+++ src/co/waynesford/fragmentbackstacks/MainActivity.java
@@ -3,7 +3,6 @@ package co.waynesford.fragmentbackstacks;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
 import android.view.View;
@@ -12,28 +11,27 @@ import android.widget.TextView;
 
 public class MainActivity extends FragmentActivity implements OnClickListener{
 
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		//setting click listeners for the tab
 		findViewById(R.id.tabA).setOnClickListener(this);
 		findViewById(R.id.tabB).setOnClickListener(this);
 		findViewById(R.id.tabC).setOnClickListener(this);
 
+		//calling click on tab a so that it's the first tab that the user see
+		mCurrentTab = findViewById(R.id.tabA);
+		onClick(mCurrentTab);
 	}
-	
+
 	@Override
 	protected void onStart() 
 	{
 		super.onStart();
-		
-		MyFragment fragment = new MyFragment();
-		fragment.setArguments("Stack A:", 1);
-		add(fragment);
 	}
 
 	@Override
@@ -42,27 +40,37 @@ public class MainActivity extends FragmentActivity implements OnClickListener{
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
+
 	@Override
 	public void onClick(View v) {
 		switch(v.getId()) {
-		case R.id.tabA: case R.id.tabB: case  R.id.tabC:
+		case R.id.tabA: case R.id.tabB: case R.id.tabC:
+
+			//deselect the previously selected tab
+			mCurrentTab.setSelected(false); 
+			v.setSelected(true); //toggles on the selected color that we set in our selector drawable
+			//grab the letter of the tab and make a new fragment and replace it on the view
 			String tab_letter = ((TextView)v).getText().toString();
 			MyFragment fragment = new MyFragment();
 			fragment.setArguments(tab_letter, 1);
 			replace(fragment);
+
+			//set tab just clicked as the new current tab
+			mCurrentTab = v; 
 			break; 
 		}
-		
 	}
-	
+
+	View mCurrentTab; 
+
+
 	private void add(Fragment fragment)
 	{
 		FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
 		fragmentTransaction.add(R.id.mainWindow, fragment);
 		fragmentTransaction.commit();
 	}
-	
+
 	private void replace (Fragment fragment)
 	{
 		FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
@@ -71,8 +79,8 @@ public class MainActivity extends FragmentActivity implements OnClickListener{
 	}
 
 
-	
-	
+
+
 
 
 }
